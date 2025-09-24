bd685cc5c45edc2641ff3e6a353cf34dd87b7cdc
keyroseh <65471405+keyroseh@users.noreply.github.com>
2025-09-23 22:40:55 -0700

    Update main.cpp
    Will add more later tonight, just to give you an idea of how main will be structured.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
-#include "student.h"
 #include <iostream>
-#include <string>
+#include <fstream> // for dat file
+#include <cctype> // for toupper, isalpha
+#include "Student.h"
 using namespace std;
+
+// Global Variables - Will add later
 
 int programming, tests, final;
 bool condition = true;
 
-int main() {
+//Functions for Choices, which will call functions from Student.cpp
+void setUpNewSemester(); // S - Call function in case/switch
+void addStudent(); // A - Call function in case/switch
+void recordProgramGrade(); // P - Call function in case/switch
+void recordTestGrade(); // T - Call function in case/switch
+void recordFinalGrade(); // F - Call function in case/switch
+void changeGrade(); // C - Call function in case/switch
+void calculateFinal(); // G - Call function in case/switch
+void outputGrades(); // O - Call function in case/switch
+//(Q) Quit - Call function to save dat file, then break loop in case/switch
+
+// dat File functions
+
+void openDat();
+void saveDat();
+
+/* Saving for setUpNewSemester
   cout << "Begin Setup for new semester!" << endl;
 
   do {
@@ -30,9 +49,109 @@ int main() {
     cin >> final;
     if (final > 1) {
       cout << "You've exceeded the max limit of final exams.\n";
-    } 
+    }
   } while (final > 1);
+*/
 
-  
-  return 0;
+void menu()
+{
+    //Print Menu
+    cout << "================ GRADE BOOK ================" << endl;
+    cout << "S). Set Up New Semester"
+    << "A). Add A Student" << endl
+    << "P). Record Programming Assignment Grade for All Students" << endl
+    << "T). Record Test Grade for All Students" << endl
+    << "F). Record Final Exam Grade for All Students" << endl
+    << "C). Change a Grade for a Particular Student" << endl
+    << "G). Calculate Final Grade" << endl
+    << "O). Output Grade Data" << endl
+    << "Q). Quit." << endl;
+    
+    while (true){ //While loop incase of input error
+    
+        string input;
+        cin >> input;
+        
+        cout << "Enter choice: ";
+        //TEST RUNS 1-3 LISTED BELOW
+       
+        // 1. If the string is longer than 1 character, it takes the first letter of the string.
+        char choice = input[0];
+        
+        // 2. Repeat if it is not a letter
+        if (!isalpha(choice))
+        {
+            cout << "ERROR! Letters only. Try again. " << endl << endl;
+            continue;
+        }
+        
+        // 3. Accepts lower case letters
+        choice = toupper(choice); //Changes lower case to upper case
+        
+        //Function Call
+        switch(choice)
+        {
+            case 'S': //Set Up New Semester
+                setUpNewSemester();
+                break;
+            case 'A': // Add a Student
+                addStudent();
+                break;
+            case 'P': // Record Program Grade
+                recordProgramGrade();
+                break;
+            case 'T': // Record Test Grade
+                recordTestGrade();
+                break;
+            case 'F': // Record Final Exam Grade
+                recordFinalGrade();
+                break;
+            case 'C': // Change a Grade
+                changeGrade();
+                break;
+            case 'G': // Calculate Final Grade
+                calculateFinal();
+                break;
+            case 'O': // Print Grades
+                outputGrades();
+                break;
+            case 'Q': // Quit
+                saveDat();
+                return;
+            default: // Any letter outside of range
+                cout << "ERROR! Enter T, F, C, G, O, or Q. Try again. " << endl << endl;
+        }
+    }
 }
+
+
+int main() {
+    
+    menu();
+    return 0;
+}
+
+//Functions for Choices, which will call functions from Student.cpp
+void setUpNewSemester() // S - Call function in case/switch
+{}
+void addStudent() // A - Call function in case/switch
+{}
+void recordProgramGrade() // P - Call function in case/switch
+{}
+void recordTestGrade() // T - Call function in case/switch
+{}
+void recordFinalGrade() // F - Call function in case/switch
+{}
+void changeGrade() // C - Call function in case/switch
+{}
+void calculateFinal() // G - Call function in case/switch
+{}
+void outputGrades() // O - Call function in case/switch
+{}
+//(Q) Quit - Call function to save dat file, then break loop in case/switch
+
+// dat File functions
+void openDat()
+{}
+void saveDat()
+{}
